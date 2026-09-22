9470666cbea8f200a1ecd1db1e4eb4965f9fa1a9
Ivan Vatlin <16368461+jeffangelion@users.noreply.github.com>
2026-09-22 22:46:56 +0300

    keep vga and disable other ports
    thanks icon#coreboot@irc.libera.chat

--- src/mainboard/randotech/k9ethv10/gma-mainboard.ads
+++ src/mainboard/randotech/k9ethv10/gma-mainboard.ads
@@ -10,14 +10,7 @@ private package GMA.Mainboard is
 
    -- FIXME: check this
    ports : constant Port_List :=
-     (DP1,
-      DP2,
-      DP3,
-      HDMI1,
-      HDMI2,
-      HDMI3,
-      Analog,
-      LVDS,
-      eDP);
+     (Analog,
+      others => Disabled);
 
 end GMA.Mainboard;
